27dd3cacbecda948ddcaa87d0aa5ebe9551010e1
AV3pml30 <39947159+aVorotnikov@users.noreply.github.com>
2023-03-13 15:56:26 +0300

    2 token stream diagrams

--- build_ast.py
+++ build_ast.py
@@ -9,6 +9,24 @@ import pathlib
 import os
 
 
+def __RenderTokenStream(diagramName, tokenList, debugInfoDir):
+    if debugInfoDir is None:
+        return
+    h = graphviz.Digraph(diagramName, format='svg')
+    h.node('0', '', shape='point')
+    i = 1
+    for token in tokenList:
+        if Token.Type.TERMINAL == token.type:
+            h.node(str(i), f"TERMINAL\ntype: {token.terminalType.name}\nstring: {token.str}", shape='box', color='red')
+        elif Token.Type.KEY == token.type:
+            h.node(str(i), f"KEY\nstring: {token.str}", shape='box', color='blue')
+        h.edge(str(i - 1), str(i))
+        i += 1
+    h.node(str(i), '', shape='point')
+    h.edge(str(i - 1), str(i))
+    h.render(directory=debugInfoDir, view=True)
+
+
 parser = ArgumentParser(prog="create_ast", description="Create AST")
 parser.add_argument("-c", "--code", dest="codeFile", help="File with code", metavar="FILE", required=True)
 parser.add_argument("-j", "--json", dest="jsonFile", help="Json file with settings", metavar="FILE", required=True)
@@ -28,19 +46,6 @@ with open(args.codeFile, 'r') as codeFile:
     code = codeFile.read()
 
 tokenList = Tokenize(code)
+__RenderTokenStream('token_stream_after_scanner', tokenList, debugInfoDir)
 tokenList = Afterscan(tokenList)
-
-if debugInfoDir is not None:
-    h = graphviz.Digraph('token_stream', format='svg')
-    h.node('0', '', shape='point')
-    i = 1
-    for token in tokenList:
-        if Token.Type.TERMINAL == token.type:
-            h.node(str(i), f"TERMINAL\ntype: {token.terminalType.name}\nstring: {token.str}", shape='box', color='red')
-        elif Token.Type.KEY == token.type:
-            h.node(str(i), f"KEY\nstring: {token.str}", shape='box', color='blue')
-        h.edge(str(i - 1), str(i))
-        i += 1
-    h.node(str(i), '', shape='point')
-    h.edge(str(i - 1), str(i))
-    h.render(directory=debugInfoDir, view=True)
+__RenderTokenStream('token_stream_after_afterscan', tokenList, debugInfoDir)
